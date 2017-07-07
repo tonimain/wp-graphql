4b1f3736fb886d63e2ab2a1b174e53181991385c
Toni Haupt <toni.haupt@codecentric.de>
2017-07-07 10:03:18 +0200

    WIP initial setup for comment connection query args -> authorEmail

--- tests/test-comment-connection-query-args.php
+++ tests/test-comment-connection-query-args.php
@@ -1,5 +1,88 @@
 <?php
 
-class WP_GraphQL_Test_Comment_Connetion_Query_Args extends WP_UnitTestCase {
+class WP_GraphQL_Test_Comment_Connection_Query_Args extends WP_UnitTestCase {
+	public $current_time;
+	public $current_date;
+	public $current_date_gmt;
+	public $admin;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->post_id = $this->factory->post->create();
+
+
+		$this->current_time     = strtotime( '- 1 day' );
+		$this->current_date     = date( 'Y-m-d H:i:s', $this->current_time );
+		$this->current_date_gmt = gmdate( 'Y-m-d H:i:s', $this->current_time );
+		$this->admin            = $this->factory->user->create( [
+			'role'       => 'administrator',
+			'user_email' => 'user@test.com',
+		] );
+
+		$this->created_comment_ids = $this->create_comments();
+
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+	}
+
+	public function createCommentObject( $args = [] ) {
+		$defaults = [
+			'comment_author'   => $this->admin,
+			'author_email'     => 'user@test.com',
+			'comment_content'  => 'Test comment content',
+			'comment_approved' => 1,
+		];
+
+		$args = array_merge( $defaults, $args );
+
+		$comment_id = $this->factory->comment->create( $args );
+
+		return $comment_id;
+	}
+
+	public function create_comments() {
+		$created_comments = [];
+
+		for ( $i = 1; $i <= 10; $i ++ ) {
+			$created_comments[ $i ] = $this->createCommentObject( [ 'comment_content' => $i ] );
+		}
+
+		return $created_comments;
+
+	}
+
+	public function testCommentConnectionQueryArgsAuthorEmail() {
+		$author_email = "user@test.com";
+
+		$query = '
+		{
+		  comments(where: {authorEmail: "admin@example.org"}) {
+		    edges {
+		      node {
+		        id
+		        content
+		      }
+		    }
+		  }
+		}
+		';
+
+		$actual = do_graphql_request( $query );
+
+		$edges = $actual['data']['comments']['edges'];
+		$this->assertNotEmpty( $edges );
+
+		$edge_count = 10;
+
+		foreach ( $edges as $edge ) {
+			$this->assertArrayHasKey( 'node', $edge );
+			$this->assertEquals( $edge['node']['content'], (string) $edge_count );
+
+			$edge_count --;
+		}
+	}
 
 }
